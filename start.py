4b7c141d4d15b9a347995d038de98b1e77e24aab
Brian Karabinchak <karabinchak.brian@gmail.com>
2018-11-24 11:41:57 -0500

    Fixed directory chooser for install

--- start.py
+++ start.py
@@ -5,8 +5,11 @@ from views.install import install
 class setup():
 	"""
 	This class serves as an object that can be used to initalize and control very basic high level information about the company.
+	Arguments:
+		None
 
-	Fields:
+
+	Attributes:
 		installPath: the install path
 
 	Methods:
@@ -47,18 +50,10 @@ class setup():
 			nothing, sets the directory that this app will save setings/data info to
 		"""
 
-		freshInstall = install.chooseDirectory('Welcome to PyBiz!  Before we continue, please pick where you want to save program data.', 'C:\Program Files\PyBiz')
-
-		# os.mkdir(self.installPath)
-		
-		# folders = ['Settings', 'Users', 'Data']
-		
-		# for folder in folders:
-		# 	os.mkdir(self.installPath"\\"+folder)
-
-		# To-Do: Everything beneath this I am realizing should be in its own fucking class window.fuck
-
-		# chooseDirecory= install.chooseDirectory()
-		
-
-		#Get Directory with validation
+		self.installWindow = install.Installer()
+	
+	def alreadyInstalled(self):
+		"""
+		Eventually, when working right, when the program runs you will just load settings and opent he main window.
+		"""
+		pass
--- views/install/install.py
+++ views/install/install.py
@@ -1,39 +1,133 @@
 #This needs to go into templates
-from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QFileDialog
 import os
+import csv
 
-class chooseDirectory(QDialog):
+class Example(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)       
+        
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('File dialog')
+        self.show()
+        
+        
+    def showDialog(self):
+
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+
+        if fname[0]:
+            f = open(fname[0], 'r')
+
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)
+
+class Installer(QMainWindow):
 	"""
-	This is a class that opens a window that allows for choosing a direcotry
+	This is a class that opens a main window and takes the user through installation
+
 	Arguments:
-		topText: String, what do you want it to say about the input
+		None
 	
 	Attributes:
-
+		presetInstallDirectory: String, pre chosen install directory
 
 	Methods:
 		getPath: String, returns with a string path.  Can be closed, or fed an additional string of the next window to open
 
 	"""
 
-	def __init__(self, topText, presetDirectory, parent=None):
-		super(chooseDirectory, self).__init__(parent)
-		self.directoryWindow = uic.loadUi('views/install/install.ui', QDialog())
-		#Check if C:\Program Files\PyBiz is available, and if not, disable the create directory button on start
-		self.directoryWindow.createDirectoryBtn.enabled = self.canCreatePath()
-		self.directoryWindow.userInputPath.text=presetDirectory
-		self.directoryWindow.userInputPath.textChanged.connect(self.canCreatePath)
-		self.directoryWindow.show()
+	currentPath = None
 
+	def __init__(self, parent=None):
+		# Load UI
+		super(Installer, self).__init__(parent)
+		self.ui = uic.loadUi('views/install/install.ui', QMainWindow())
+
+		# Connect buttons and trigger events
+		self.ui.next.enabled = self.canCreatePath()
+		self.ui.userInputPath.textChanged.connect(self.canCreatePath)
+		self.ui.chooseDirectoryBtn.clicked.connect(self.openFileDialog)
+		self.ui.next.clicked.connect(self.setUpDirectory)
+
+		# Show Window
+		self.ui.show()
 	
 	def canCreatePath(self):
 		"""
-		This function determins if the create button should be enabled
+		self.canCreatePath: function determines if the next button is enabled/if the path is viable
 		Arguments:
-			path: string, of path to put directory
+			self: entire class
 		Returns:
-			Boolean, true if you can create the directory there, false if you
+			Nothing, sets the next button to enabled or not depending on if the path is viable
 		"""
-		return not (os.path.isdir(self.directoryWindow.userInputPath.text))
+		self.currentPath = self.ui.userInputPath.text()
+
+		try:
+			print("checking path of: ", self.currentPath)
+			pathViable = not (os.path.isdir(self.currentPath))
+			self.ui.next.setEnabled(pathViable)
+		except TypeError:
+			self.ui.next.setEnabled(False)
+
+	def openFileDialog(self):
+		"""
+		openFileDialog: function is triggered by the chooseDirectoryBtn QToolButton
+		Arguments:
+			self: entire class
+		Returns:
+			Nothing, sets the userInputPath to the new chose path
+		"""
+		# Get directory for install from user
+		self.currentPath = QFileDialog.getExistingDirectory(self, 'Choose Parent Directory')
+		self.currentPath = self.currentPath.replace('/','\\') + "\\" + "PyBiz"
+		
+		# Set it and check if next button is ok
+		self.ui.userInputPath.setText(self.currentPath)
+		self.canCreatePath()
+
+	def setUpDirectory(self):
+		"""
+		nextInstallWindow: function sets up directory and opens up the main window
+		Arguments:
+			self: entire class
+		Returns:
+			Nothing, creates directory for PyBiz and moves foward
+		"""
+		print("making setUpDirectory in: ", self.currentPath)
+
+		os.mkdir(self.currentPath)
+
+		subfolders = ['data', 'bin', 'settings']
+		for folder in subfolders:
+			os.mkdir(self.currentPath + '\\' + folder)
+
+		settings = self.currentPath + '\\' + 'settings' + '\\settings.csv'
+		with open(settings, 'w') as csvfile:
+			settingFields = ['Name', 'Company', 'Email']
+			writer = csv.DictWriter(csvfile, fieldnames=settingFields)
+
+			writer.writeheader()
+			writer.writerow({'Name': 'Brian', 'Company': 'PSI Inc', 'Email': 'karabinchak.brian@gmail.com'})
